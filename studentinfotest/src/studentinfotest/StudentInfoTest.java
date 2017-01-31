05617dd055124a78ea035ab550edf9548abf7fb6
bystc <598296757@qq.com>
2017-01-31 22:04:25 +0800

    2017.1.31

--- studentinfotest/src/studentinfotest/StudentInfoTest.java
+++ studentinfotest/src/studentinfotest/StudentInfoTest.java
@@ -1,5 +1,6 @@
 package studentinfotest;
 import java.io.*;
+import java.util.*;
 /*
  * 有五个学生，每个学生有3门课的成绩
  * 从键盘输入以上数据（包括姓名，三门课成绩）
@@ -15,16 +16,93 @@ import java.io.*;
  * 所以可以使用TreeSet
  * 3.将集合的信息写入到一个文件中。
  * 
- * 
- * 
- * 
- * 
- * 
- */
 
-public class StudentInfoTest {
-	public static void main(String[] args){
+ */
+class Student implements Comparable<Student>{
+	private String name;
+	private int ma,cn,en;
+	private int sum;
+	Student(String name,int ma,int cn,int en){
+		this.name=name;
+		this.ma=ma;
+		this.cn=cn;
+		this.en=en;
+		sum=ma+cn+en;
+	}
+	public String getName(){
+		return name;
 		
+	}
+	public int getSum(){
+		return sum;
+		
+	}
+	public int hasCode(){
+		return name.hashCode()+sum*78;
+		
+	}
+	public boolean equals(Object obj){
+		if(!(obj instanceof Student))
+			throw new ClassCastException("类型不匹配");
+		Student s=(Student)obj;
+		return this.name.equals(s.name)&&this.sum==s.sum;
+	}
+	@Override
+	public int compareTo(Student s) {
+		int num=new Integer(this.sum).compareTo(new Integer(s.sum));
+		if(num==0)
+			return this.name.compareTo(this.name);
+		return num;
+	}
+	public String toString(){
+		return "student["+name+", "+ma+", "+cn+", "+en+"] ";
+	}
+}
+
+class StudentInfoTool{
+	public static Set<Student>getStudent() throws IOException{
+		return getStudent(null);
+	}
+	public static Set<Student>getStudent(Comparator<Student>cmp) throws IOException{
+		BufferedReader bufr=new BufferedReader(new InputStreamReader(System.in));
+		String line=null;
+	
+		Set<Student>stus=null;
+		if(cmp==null)
+			stus=new TreeSet<Student>();
+		else
+			stus=new TreeSet<Student>(cmp);
+		
+		while((line=bufr.readLine())!=null)
+		{
+			if("over".equals(line))
+				break;
+			String[]info=line.split(",");
+			Student stu=new Student(info[0],Integer.parseInt(info[1]),
+					Integer.parseInt(info[2]),
+					Integer.parseInt(info[3]));
+			stus.add(stu);
+		}
+		bufr.close();
+		return stus;
+	}
+	public static void writeFile(Set<Student> stus)throws IOException{
+		BufferedWriter bufw=new BufferedWriter(new FileWriter("stuinfo.txt"));
+		for(Student stu:stus){
+			bufw.write(stu.toString()+"\t");
+			bufw.write(stu.getSum()+"");
+			bufw.newLine();
+			bufw.flush();
+		}
+		bufw.close();
+	}
+}
+public class StudentInfoTest {
+	public static void main(String[] args) throws IOException{
+		
+		Comparator<Student>cmp=Collections.reverseOrder();
+		Set<Student>stus=StudentInfoTool.getStudent(cmp);
+		StudentInfoTool.writeFile(stus);
 	}
 	
 }
