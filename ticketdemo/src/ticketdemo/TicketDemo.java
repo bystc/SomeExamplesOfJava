c3f2aebd339262053fca55cbee89bcc4a4ccc4ee
bystc <598296757@qq.com>
2017-02-12 12:24:38 +0800

    2017.2.12

--- ticketdemo/src/ticketdemo/TicketDemo.java
+++ ticketdemo/src/ticketdemo/TicketDemo.java
@@ -1,22 +1,35 @@
 package ticketdemo;
 class Ticket implements  Runnable{//extends Thread{
 	private  int  ticket=100;
-	Object obj=new Object();
+	//Object obj=new Object();
 	public void run(){
 		while(true)				
 		{	
-			synchronized(obj){
-				if(ticket>0){
-					try{
-						Thread.sleep(10);
-					}catch(Exception e){
-						throw new RuntimeException();
-					}
-			
-			System.out.println(Thread.currentThread().getName()+"sale: " + ticket--);
-			}
-			}
+			//synchronized(obj){
+//				if(ticket>0){
+//					try{
+//						Thread.sleep(10);
+//					}catch(Exception e){
+//						throw new RuntimeException();
+//					}
+//			
+//			System.out.println(Thread.currentThread().getName()+"sale: " + ticket--);
+//			}
+			this.show();
 		}
+		
+	}
+	public synchronized void show()
+	{
+		if(ticket>0){
+			try{
+				Thread.sleep(10);
+			}catch(Exception e){
+				throw new RuntimeException();
+			}
+	
+			System.out.println(Thread.currentThread().getName()+"sale: " + ticket--);
+	}
 	}
 }
 public class TicketDemo {
